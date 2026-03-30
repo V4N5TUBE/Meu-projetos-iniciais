411bce44bb43546dc69802dff16fa49c0a94dde2
Vinicius Antunes <viniciusantunes250@gmail.com>
2026-03-30 13:26:03 -0400

    att

--- codigos base/break.py
+++
@@ -1,7 +0,0 @@
-nomes = ["PM3", "Alura", "Latam", "Outros"]
-
-for nome in nomes:
-   if nome == "Alura":
-       print(f"Nome encontrado: {nome}. Saindo do laço.")
-       break
-   print(nome)
--- codigos base/countinue.py
+++
@@ -1,7 +0,0 @@
-nomes = ["PM3", "Alura", "Latam", "Outros"]
-
-for nome in nomes:
-   if nome == "Alura":
-       print("Ignorando Alura.")
-       continue
-   print(f"Nome: {nome}")
