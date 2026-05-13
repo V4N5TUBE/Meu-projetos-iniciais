4e4d7bd8194f48c0f0017323458e99262a26b068
Vinicius Antunes <viniciusantunes250@gmail.com>
2026-05-13 15:06:54 -0400

    att aplic django

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from galeria.views import index
+from django.conf import settings
+from django.conf.urls.static import static
+from galeria.views import index, imagem
 
 urlpatterns = [
-    path('',index),
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
